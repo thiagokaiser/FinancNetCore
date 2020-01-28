ecd7992f575b4aeaa836cae90b7217c223627923
unknown <thiago.kaiser@hotmail.com>
2020-01-28 16:24:37 -0300

    identity 1.1

---
+++ FinancNetCore/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using Api.Models;
+using Core.Models;
+using Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<AppUser> UserMgr { get; }
+        private SignInManager<AppUser> SignInMgr { get; }
+
+        public AccountController(UserManager<AppUser> userManager,
+                                 SignInManager<AppUser> signInManager)
+        {
+            UserMgr = userManager;
+            SignInMgr = signInManager;
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            try
+            {
+                ViewBag.Message = "User already registered";
+
+                AppUser user = await UserMgr.FindByNameAsync("testuser");
+                if (user == null)
+                {
+                    user = new AppUser();
+                    user.UserName = "testuser";
+                    user.Email = "a@a.com";
+                    user.FirstName = "Thiago";
+                    user.LastName = "Kaiser";
+
+                    IdentityResult result = await UserMgr.CreateAsync(user, "Test@123");
+                    ViewBag.Message = "User was created";
+                }
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            var result = await SignInMgr.PasswordSignInAsync("testuser","Test@123", false, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Result = "Result is: " + result.ToString();
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await SignInMgr.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
